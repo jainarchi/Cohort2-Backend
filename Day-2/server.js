d74329abfa0e05f24c9a352fdece7e6a1104aa2d
jainarchi <imarchijain2405@gmail.com>
2026-01-24 21:09:55 +0530

    day-2

---
+++ Day-2/server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json())          // by def server is not capable to read req.body that's why we use this middleware 
+
+
+
+let notes = [
+    {
+        "title" : "title 1",
+        "description" : "desp 1 "
+    },
+    {
+        "title" : "title 2",
+        "description" : "desp 2 "
+    } ,
+    {
+        "title" : "title 3",
+        "description" : "desp 3 "
+    }
+]       
+
+
+
+
+app.post('/notes' , (req , res) =>{               // post api - create resource of data
+    notes.push(req.body);
+
+    res.send('note saved successfully') 
+})
+
+
+
+app.get('/notes' , (req , res) =>{                       // get api - send data
+    res.send(notes);
+})
+
+
+
+app.delete('/notes/:index' , (req , res) => {          
+    delete notes[req.params.index]                       // replace with null
+    res.send('delete successfully ')
+
+})
+
+
+
+app.patch('/notes/:index' , (req , res) =>{
+
+   const{ title , description } = req.body
+    
+   description ? notes[req.params.index].description = req.body.description
+               : notes[req.params.index].title = req.body.title ;
+
+    res.send("modified successfully") 
+})
+
+
+
+app.put('/notes/:index' , (req , res) =>{
+     notes[req.params.index] = req.body ;
+     res.send("update completely")
+})
+
+
+
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+// create rest api for notes
--- myNotes.md
+++ myNotes.md
@@ -4,3 +4,13 @@ package-lock.json - contains all dependences ie our package depends on further p
 
 node-module - all i packages present inside this.
 
+
+// day - 2  
+- REST API - transfer data in JSON
+- partial update in note - patch api
+- delete - replace with null 
+
+
+Data send from frontend -
+- params : very small data
+- body : large data 
