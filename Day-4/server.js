f0c44ab7f983379d1326b939a086186b996c946e
jainarchi <imarchijain2405@gmail.com>
2026-01-29 02:22:47 +0530

    Day-4_DB

--- Day-4/server.js
+++ Day-4/server.js
@@ -3,17 +3,27 @@
 
 
 const app = require("./src/app")
-const mongoose = require('mongoose');
 require('dotenv').config();
 
+const mongoose = require('mongoose');
 
 const connectWithDB = () =>{
-    mongoose.connect(process.env.mongoDB_URL/day-6)
+    mongoose.connect(process.env.mongoDB_URL)
+    .then(() =>{
+        console.log('DB connected')
+    })
 }
 
 connectWithDB();
 
 
+
 app.listen(3000 , () =>{
     console.log('server on port 3000');
 })
+
+
+
+
+// mongoose.connect()  - connect Server to DB
+// cluster/db   -  create if not present then connect
