b98462c3c3f91d13bca761835283172184500bb6
jainarchi <imarchijain2405@gmail.com>
2026-02-08 01:42:00 +0530

    login api

--- Day-12/routes/authRoute.js
+++ Day-12/routes/authRoute.js
@@ -2,12 +2,10 @@ const express = require('express')
 const userModel  = require('../models/users.model')
 const authRouter = express.Router()
 const JWT = require('jsonwebtoken')
+const crypto = require('crypto')                  // nodejs build-in core module
 
 
-/* 
-    POST  /api/auth/register
-    controller - func run on req 
-*/
+
 
 authRouter.post('/register' , async (req , res) =>{
     const {username , email , password} = req.body
@@ -21,10 +19,12 @@ authRouter.post('/register' , async (req , res) =>{
     })
    }
 
+   const hash = crypto.createHash('md5').update(password).digest('hex')
+
    const user = await userModel.create({
     username,
     email,
-    password
+    password : hash
    })
 
    const token = JWT.sign({
@@ -45,6 +45,43 @@ authRouter.post('/register' , async (req , res) =>{
 
     
 })
+
+
+authRouter.post('/login' , async (req , res) =>{
+   const {email , password} = req.body
+
+   const user = await userModel.findOne({email})
+
+   if( !user ){
+    return res.status(404)
+    .json({
+        message : "User not found"
+    })
+   }
+
+   const isPasswordMatch = user.password === crypto.createHash('md5').update(password).digest('hex')
+   
+   if(! isPasswordMatch ){
+    return res.status(401)
+    .json({
+        message: "Invalid Password"
+    })
+   }
+
+   
+   const token = JWT.sign({
+    id : user._id
+   }, process.env.JWT_SECRET)
+
+
+   res.cookie('jwt_token' , token)
+
+   res.status(200)
+   .json({
+      message: "logged in successfully"
+   })
+})
+
 
 
 module.exports = authRouter
