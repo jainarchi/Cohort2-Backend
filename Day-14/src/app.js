713a2c7e70e4aa362904409e298f116d12ab0e9c
jainarchi <imarchijain2405@gmail.com>
2026-02-10 23:56:40 +0530

    routes

--- Day-14/src/app.js
+++ Day-14/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const authRouter = require('./routes/authRoutes')
+const authRouter = require('./routes/auth.routes')
 
 
 
